e8d226dde4250bb8c10aa491d75dbfd4d901c9d6
Johannes Valter-Lax <jvl@Johanness-Mac-mini.local>
2025-07-21 19:12:24 +0200

    Add test package that never existed

--- test-demo-imports.js
+++ test-demo-imports.js
@@ -1,8 +1,9 @@
 // Test file for demonstrating publishing of non-test packages
 // These imports don't exist and aren't in our test manifest
 
-const superReactHelper = require('super-react-helper');  // Should be publishable
+const superReactHelper = require('super-react-helper');  // Should be publishable (deleted package)
 import awesomeUtils from 'awesome-frontend-utils';  // Should be publishable
 import('demo-dynamic-package');  // Should be publishable
+const neverExisted = require('totally-new-package-never-existed-12345'); // Should be publishable (never existed)
 
 console.log("This is just a demo file for testing dependency scanning and publishing"); 
